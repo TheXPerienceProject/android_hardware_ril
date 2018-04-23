8181d06581a0c930eeca0e58c34c7f56c2e1b198
klozz <klozz707@gmail.com>
2018-04-22 20:05:27 -0400

    ril: allow to build ril in GSI treble
    Signed-off-by: klozz <klozz707@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,7 @@ RIL_PATH := $(call my-dir)
 ifeq ($(RIL_PATH),$(call project-path-for,ril))
 include $(call first-makefiles-under,$(RIL_PATH))
 endif
+
+ifeq ($(XPERIENCE_EXPORT_TREBLE),true)
+include $(call first-makefiles-under,$(RIL_PATH))
+endif
